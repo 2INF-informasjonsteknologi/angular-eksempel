96b76a2e0f8cf056d525d569df982d686bbe816b
Daniel Kristoffersen <dk@dk-media.no>
2023-03-21 13:12:47 +0100

    Oppstart av prosjekt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrontpageComponent } from './frontpage/frontpage.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontpageComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
